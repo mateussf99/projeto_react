9ff7a4f04ed69e571c98f745e7243ed3bf8ea034
Mateus <mateussf2208@hotmail.com>
2023-08-11 10:47:23 -0300

    tirando o teste.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Home from './pages/home';
 import Cadastro from './pages/cadastro'
 import Selecao from './pages/selecao_materias';
-import Teste from './pages/teste';
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/teste" element={<Teste />} />
         <Route path="/selecao" element={<Selecao />} />
       </Routes>
     </Router>
